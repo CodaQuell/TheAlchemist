3ab600f242181c4eecef71f356f6e9c7b6a5a334
SEJD103 <143581155+SEJD103@users.noreply.github.com>
2024-08-26 15:08:09 +1000

    Update enemyClass.py

--- main/classes/enemyClass.py
+++ main/classes/enemyClass.py
@@ -1,15 +1,17 @@
 import pygame
 from pygame.locals import *
-
-import random
 from random import *
 
 class Enemy (pygame.sprite.Sprite):
-    def __intit__(self,health,damage,speed,spec_damage,exp,gold):
-        
+    def __init__(self,health,damage,speed,spec_damage,exp,gold):
+        super(Enemy,self).__init__()
         # Defining attributes for enemy, health, damage dealt to player on succesful attack,
         # movement speed, and damage from any special atrributes the enemy may have(e.g. poison).
         # Also the amount of exp and gold said enemy will drop
+        self.surface = pygame.Surface((20,20))
+        self.rect = self.surface.get_rect()
+        self.surface.fill ("blue")
+        self.rect.center = [600,300]
         self.health = health
         self.damage = damage
         self.speed = speed
@@ -17,21 +19,27 @@ class Enemy (pygame.sprite.Sprite):
         self.exp = exp
         self.gold = gold
 
-    def lootdrop(gold,exp):
-        gold = random.randint((gold-5),(gold+5))
-        exp = random.randint((exp-50),(exp+50))
+    def lootdrop(self,keys,gold,exp,frames):
+        gold = randint((gold-5),(gold+5))
+        exp = randint((exp-50),(exp+50))
         drops = (gold, exp) 
-        print (drops)
+        if frames == 1:
+
+            print (drops)
+        
         
         
 
         
-    def update(self):
+    def update(self,keys,lootdrop,gold,exp):
         if self.health >= 0:
             self.kill
+            lootdrop(self,gold,exp,-1)
             #if we decide to do a loot system, make a "drop loot" function and run it here
 
 
 
+
+    
 
 
