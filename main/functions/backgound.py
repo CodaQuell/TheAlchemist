e8a4bb7e56cac8243cb3e05e9be6b69b5fc28b27
Joss <122181428+CodaQuell@users.noreply.github.com>
2024-09-04 13:11:10 +1000

    Update backgound.py

--- main/functions/backgound.py
+++ main/functions/backgound.py
@@ -12,15 +12,22 @@ H = app.winfo_screenheight()
 screen = pygame.display.set_mode((W,H))
 
 #images path
-image_path = r"C:\Users\joss\Downloads\tower-game-main\tower-game-main\main\assets\background0.png"
-
+image_list=[]
+current_image = 0
+image_list.append("C:\\Users\\josso\Downloads\\tower-game-main\\tower-game-main\\main\\assets\\background0.png")
+image_list.append("C:\\Users\\josso\Downloads\\tower-game-main\\tower-game-main\\main\\assets\\backgound1.png")
 #loads selected image from image path
 def background(image,W,H,screen):
     #loads the image
-    image = pygame.image.load(image_path)
+    image = pygame.image.load(image_list[current_image])
     #scales the image to fit the screen, even tho all back ground art will be made for 16:9 ratio
     scaled_image = pygame.transform.scale(image, (W, H))
     #draws the the screen
     screen.blit(scaled_image, (0, 0))
 
+def next_image(image,W,H,screen):
+    global current_image
+    if len(image_list)>current_image+1:
+        current_image +=1
+    background(image,W,H,screen)
 
