657dc8cc0871ce47989d647bf2ddd0eb7a20015e
SEJD103 <143581155+SEJD103@users.noreply.github.com>
2024-09-09 15:10:34 +1000

    nuh uh

--- main/classes/playerClass.py
+++ main/classes/playerClass.py
@@ -3,6 +3,8 @@ import pygame
 from pygame.locals import *
 import random
 from random import *
+import time
+
 
 #inits playerclass
 class Player(pygame.sprite.Sprite):
@@ -13,9 +15,32 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.surface.get_rect()
         self.surface.fill ("white")
         self.rect.center = [300,300]
-        self.speed = 3
+        self.speed = 7
         self.screenH = screenH
         self.screenW = screenW
+
+
+      
+    def attack(self,charge):
+        charge = 10
+        state = pygame.mouse.get_pressed()
+        if state == 1:
+            hitrange = Collider(2,10)
+            charge -= 1
+            pygame.time.wait(5000)
+            hitrange.kill()
+        if state == 2:
+            hitrange = Collider(4,15)
+            charge -= 2
+            pygame.time.wait(5000)
+            hitrange.kill()
+
+        
+
+
+            
+            
+        
 
     #checks if keys are pressed, then moves accoss x,y plane in accouding direction
     def update(self, keys_pressed):
@@ -27,6 +52,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= self.speed
         if keys_pressed[K_d] or keys_pressed[K_RIGHT]:
             self.rect.x += self.speed
+        
 
         #prevents the player from leaving the confines of the screen
         if self.rect.bottom > self.screenH:
@@ -38,5 +64,16 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right > self.screenW:
             self.rect.right = self.screenW        
 
+            
+class Collider(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super(Collider,self).__init__()
+        x = x
+        y = y
+        vert = (x,y)
+        #sets basic player stats
+        self.surface = pygame.Surface((vert))
+        self.rect = self.surface.get_rect()
+        self.surface.fill("white")
             
 
