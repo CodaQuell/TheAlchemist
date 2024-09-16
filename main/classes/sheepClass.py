16338c23b3c35a9e97f3fd76495739522d702f16
Joss <122181428+CodaQuell@users.noreply.github.com>
2024-09-16 14:31:48 +1000

    Update sheepClass.py

--- main/classes/sheepClass.py
+++ main/classes/sheepClass.py
@@ -1,11 +1,16 @@
-from classes.enemyClass import *
+import pygame
+from pygame.locals import *
 
-class Sheep (Enemy):
-    def __init__(self,health,speed):
-        super(Sheep,self).__init__(health,damage=0,speed=speed,spec_damage=0,exp=55,gold=6)
-        #self.health = health
-        #self.speed = speed
+import random
+from random import *
+
+#creates wall class with a health, a surface, and a spawn draw point
+class Sheep(pygame.sprite.Sprite):
+    def __init__(self,health,speed,center):
+        super(Sheep,self).__init__()
+        self.health = health
+        self.speed = speed
         self.surface = pygame.Surface((40,20))
         self.rect = self.surface.get_rect()
         self.surface.fill ("green")
-        self.rect.center = [600,600]
+        self.rect.center = center
