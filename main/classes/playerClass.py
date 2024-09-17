6e80e1362ae9ab4761aeab4f7c7e8d3b508cf418
SEJD103 <143581155+SEJD103@users.noreply.github.com>
2024-09-18 07:34:44 +1000

    Update playerClass.py

--- main/classes/playerClass.py
+++ main/classes/playerClass.py
@@ -6,6 +6,27 @@ from random import *
 import time
 
 
+class Collider(pygame.sprite.Sprite):
+    def __init__(self,duration, x,y, width, height):
+        super(Collider, self).__init__()
+        self.surface = pygame.Surface([width, height])
+        self.start = time.time()
+        self.duration = duration
+        self.rect = self.surface.get_rect()
+        self.x = x
+        self.y = y
+        
+    
+        self.surface.fill ("green")
+
+    
+    def update(self, *args):
+        if time.time()-self.start >self.duration:
+            self.kill()
+
+        
+
+
 #inits playerclass
 class Player(pygame.sprite.Sprite):
     def __init__(self,screenH,screenW):
@@ -15,32 +36,9 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.surface.get_rect()
         self.surface.fill ("white")
         self.rect.center = [300,300]
-        self.speed = 7
+        self.speed = 10
         self.screenH = screenH
         self.screenW = screenW
-
-
-      
-    def attack(self,charge):
-        charge = 10
-        state = pygame.mouse.get_pressed()
-        if state == 1:
-            hitrange = Collider(2,10)
-            charge -= 1
-            pygame.time.wait(5000)
-            hitrange.kill()
-        if state == 2:
-            hitrange = Collider(4,15)
-            charge -= 2
-            pygame.time.wait(5000)
-            hitrange.kill()
-
-        
-
-
-            
-            
-        
 
     #checks if keys are pressed, then moves accoss x,y plane in accouding direction
     def update(self, keys_pressed):
@@ -52,7 +50,6 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= self.speed
         if keys_pressed[K_d] or keys_pressed[K_RIGHT]:
             self.rect.x += self.speed
-        
 
         #prevents the player from leaving the confines of the screen
         if self.rect.bottom > self.screenH:
@@ -63,17 +60,5 @@ class Player(pygame.sprite.Sprite):
             self.rect.left = 0
         if self.rect.right > self.screenW:
             self.rect.right = self.screenW        
-
-            
-class Collider(pygame.sprite.Sprite):
-    def __init__(self,x,y):
-        super(Collider,self).__init__()
-        x = x
-        y = y
-        vert = (x,y)
-        #sets basic player stats
-        self.surface = pygame.Surface((vert))
-        self.rect = self.surface.get_rect()
-        self.surface.fill("white")
             
 
