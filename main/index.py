cf73e48e79d6a5c2f49f744a4fa959a4f5fdb904
SEJD103 <143581155+SEJD103@users.noreply.github.com>
2024-09-18 07:34:14 +1000

    Update index.py

--- main/index.py
+++ main/index.py
@@ -1,90 +1,70 @@
-"""
-ideas for future:
-  tile map:
-    an idea to make puting items in place would be to make a grind on the screen, like we have now with H and W, but using the values 160 and 90 (or 1600 by 900)
-    we would use 160:90 because all the monitors that we work on should in theory have a 16:9 aspect ratio, where as the resolution for some of them is different. 
-    using with we would make all our maps with those dimetions, and it would help alot with knowing where to place items like walls. 
-  image shifter:
-    every time we go up or down a level we just render a new image from the assets folder, set the playes stats in a file or variable.
-    we would then kill all the entities and redraw the new ones. 
-    an issue with this is that if the player chose to go back down a level of the tower (if thats and option), the entities from that level would be redrawn.
-    this could lead to easy loot drops, entity farming, braking game progress.
-  attacks func for player:
-    not sure how to go about making this one yet, will continue to expand on ideas...
 
-"""
+"THIS DOES NOT WORK YET"
 
 #3rd party libaries import
 import sys
-#import tkinter
+import tkinter
 import pygame
 from pygame.locals import *
 import random
 from random import *
+import time
+import math
 
 #import classs, funcs, and assets to the index
-from classes.playerClass import Player
-from classes.enemyClass import Enemy 
+from classes.playerClass import *
+from classes.enemyClass import *
 from classes.enemyClass import *
 from classes.sheepClass import * 
-from classes.wallClass import Wall
+from classes.wallClass import *
+from classes.newLevelClass import *
+from classes.player import player
+
+
+
 from functions.backgound import background
 from functions.backgound import current_image
 from functions.backgound import next_image
+from functions.backgound import prev_image
+from functions.killFunc import entityKill
+
+from functions.spriteGroups import *
+
+from levels.level1 import level1
+from levels.level2 import level2
+from levels.level3 import level3
 
 # getting height and width for the game window
 # an included module, hopefully school computers have it.
-#app = tkinter.Tk()
-#W = app.winfo_screenwidth()
-#H = app.winfo_screenheight()
-W = 2560
-H = 1440
-
+app = tkinter.Tk()
+W = app.winfo_screenwidth()
+H = app.winfo_screenheight()
 
 #init pygame
 pygame.init()
 
 #geting tick rate for the game time
-fps = 30
+fps = 60
 fpsClock = pygame.time.Clock()
 
 #inits the sceen with the the H and W defined earlier
 screen = pygame.display.set_mode((W,H))
 
-#init player from Player class
-player = Player(H,W)
 
-#inits other sprites using
-enemy = Enemy(20,2,3,0,60,34)
-sheep = Sheep(5,2)
-wall = Wall((randint(20,H),randint(20,W)))
-
-#create sprite group
-spriteGroup1 = pygame.sprite.Group()
-spriteGroup2= pygame.sprite.Group()
-spriteGroup3 = pygame.sprite.Group()
-
-#add player+others to respective sprite groups to sprite group
-#must be in sprite group1 to be drawn onto screen
-spriteGroup1.add(player)
-spriteGroup1.add(wall)
-spriteGroup1.add(enemy)
-spriteGroup1.add(sheep)
-
-#non playerable entites, animals or monsters
-spriteGroup2.add(enemy)
-spriteGroup2.add(sheep)
-
-#sprite group 3 will be for walls or solid objects
-spriteGroup3.add(wall)
-
+#checks what level is loaded
+level = 1
+level1()
 #stores players location on x,y plane, used to set position apon collision 
 tempPlayerPos = player.rect.center
+
+
 
 # Game loop
 while True:
   #gets selected background image from background func
   background(current_image,W,H,screen)
+
+  
   
   #checks for esc key pressed to exit game
   for event in pygame.event.get():
@@ -96,13 +76,51 @@ while True:
 
         pygame.quit()
         sys.exit()
-    
 
-    player.attack(0)
+        
+
+     
+    tempDur = .3    
+    if event.type == MOUSEBUTTONDOWN:
+      playercollider_left = Collider(tempDur,player.rect.left,player.rect.centery,20,10)
+      playercollider_right = Collider(tempDur,player.rect.right,player.rect.centery,40,10)
+      playercollider_up = Collider(tempDur,player.rect.centerx,player.rect.top,10,20)
+      playercollider_down = Collider(tempDur,player.rect.centerx,player.rect.bottom,10,40)
+
+      tempMouse = pygame.mouse.get_pos()
+      degrees_x = tempMouse[0] - tempPlayerPos[0]
+      degrees_y = tempMouse[1] - tempPlayerPos[1]
+
+      # Calculate the angle in radians
+      angle_radians = math.atan2(degrees_x, degrees_y)
+
+      # convert to degrees
+      angle_degrees = math.degrees(angle_radians)
+
+      if -45 < angle_degrees < 45:
+        spriteGroup1.add (playercollider_down)
+
+      if 45 < angle_degrees < 135:
+        spriteGroup1.add (playercollider_right)
+
+      
+      if 135 < angle_degrees and angle_degrees < -135:
+        spriteGroup1.add (playercollider_up)
+
+      if -135 < angle_degrees and angle_degrees > -45:
+        spriteGroup1.add (playercollider_left)
+        
+        
+
+      
+          
+    
 # updating the game when keystrokes are initiated
   keys = pygame.key.get_pressed()
   for sprite in spriteGroup1:
     sprite.update(keys)
+
+  
    
   # checking for a wall collide, then stopping the player if there is
   wallTouch = False    
@@ -126,6 +144,34 @@ while True:
        en.health -= 1
        #next_image(current_image,W,H,screen)
 
+  #adds collition with newLevel objects, calles enitiy kill func, loads new level enities
+  for lvl in spriteGroup4:
+    if pygame.sprite.collide_rect(player,lvl):
+      if lvl.num == 1:
+        level += 1
+        entityKill()
+        player.rect.center = (300,300)
+        #loads image from from image loader func today
+        next_image(current_image,W,H,screen)
+        if level == 1:
+          level1()
+        elif level == 2:
+          level2()
+        elif level == 3:
+          level3()
+        
+        
+      elif lvl.num == 0:
+        level -= 1
+        entityKill()
+        player.rect.center = (300,300)
+        prev_image(current_image,W,H,screen)
+        if level == 1:
+          level1()
+        elif level == 2:
+          level2()
+        elif level == 3:
+          level3()
        
   #updates the screen ever 60 ticks. This is set in fpsClock
   pygame.display.update()
