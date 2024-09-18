73574eb9e59ad2e7bc585256778994296d0c8e0b
Joss <122181428+CodaQuell@users.noreply.github.com>
2024-09-19 01:52:37 +1000

    Delete main directory

--- main/classes/player.py
+++
@@ -1,9 +0,0 @@
-from classes.playerClass import *
-import tkinter
-
-app = tkinter.Tk()
-W = app.winfo_screenwidth()
-H = app.winfo_screenheight()
-
-#init player from Player class
-player = Player(H,W)
--- main/functions/killFunc.py
+++
@@ -1,6 +0,0 @@
-from functions.spriteGroups import *
-
-#kills all enities thta are in sprite group 5, should be everything but player
-def entityKill():
-  for enitiy in spriteGroup5:
-    enitiy.kill()
